73ad6df1caa497af44dda5df098d29a4170cd230
Evgeni Sladkowski <kapodes@gmail.com>
2020-09-22 11:22:05 +0200

    adding github actions to build cli

---
+++ .github/scripts/native/graalvm-win.sh
@@ -0,0 +1,7 @@
+curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.2.0/graalvm-ce-java11-windows-amd64-20.2.0.zip -o graalvm.zip
+unzip -q graalvm.zip -d graalvm
+rm graalvm.zip
+mv graalvm/graalvm-ce-java11-20.2.0/* graalvm/
+rm -rf graalvm/graalvm-ce-java11-20.2.0
+graalvm/bin/gu.cmd install -n native-image
+graalvm/bin/native-image.cmd --version
