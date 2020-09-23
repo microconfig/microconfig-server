1d6a809fc1837583d89af37b75bfc663977c79f3
Evgeni Sladkowski <kapodes@gmail.com>
2020-09-23 12:15:55 +0200

    official release 0.1.3

--- cli/src/main/java/io/microconfig/cli/commands/VersionCommand.java
+++ cli/src/main/java/io/microconfig/cli/commands/VersionCommand.java
@@ -8,7 +8,7 @@ public class VersionCommand extends Command {
 
     @Override
     public int execute() {
-        System.out.println("Version 0.1.2");
+        System.out.println("Version 0.1.3");
         return 0;
     }
 }
