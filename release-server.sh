6b251942cef6805b036e01b3d2df966963b49988
Evgeni Sladkowski <kapodes@gmail.com>
2020-10-02 15:25:15 +0200

    minor docker build changes

--- release-server.sh
+++ release-server.sh
@@ -1,9 +1,10 @@
-TAG="latest_alpha"
+TAG="latest_beta"
 
 if [ -n "$1" ]
 then
     TAG=$1
 fi
 
-docker build -t microconfig/server:"$TAG" -f server/Dockerfile server
+docker build -t microconfig/server:"$TAG" -t microconfig/server:latest -f server/Dockerfile server
 docker push microconfig/server:"$TAG"
+docker push microconfig/server:latest
