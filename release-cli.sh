b49f3b403641a1067e66e44d44d190e770eaa3af
Evgeni Sladkowski <kapodes@gmail.com>
2020-10-07 14:00:37 +0200

    cli docker release script update

--- release-cli.sh
+++ release-cli.sh
@@ -1,2 +1,12 @@
-docker build -t microconfig/server-cli:latest_beta -f cli/Dockerfile cli
-docker push microconfig/server-cli:latest_beta
+TAG="latest_beta"
+
+if [ -n "$1" ]
+then
+    TAG=$1
+fi
+
+./gradlew shadowJar -p cli
+
+docker build -t microconfig/server-cli:"$TAG" -t microconfig/server-cli:latest -f cli/Dockerfile cli
+docker push microconfig/server-cli:"$TAG"
+docker push microconfig/server-cli:latest
