941bc9bb8243f95cdc9a732c52317fa0e4626e64
Evgeni Sladkowski <kapodes@gmail.com>
2020-10-07 13:13:57 +0200

    added jsr305 dependency for Null annotations

--- release-server.sh
+++ release-server.sh
@@ -5,6 +5,7 @@ then
     TAG=$1
 fi
 
+./gradlew build -p server
 docker build -t microconfig/server:"$TAG" -t microconfig/server:latest -f server/Dockerfile server
 docker push microconfig/server:"$TAG"
 docker push microconfig/server:latest
