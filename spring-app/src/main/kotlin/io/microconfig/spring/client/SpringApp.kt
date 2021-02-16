5085461857f52def3ae50a5d24a679c7ff91c3de
Evgenii Sladkovskii <kapodes@gmail.com>
2021-02-16 20:17:39 +0100

    passing vars to server in spring client

--- spring-app/src/main/kotlin/io/microconfig/spring/client/SpringApp.kt
+++ spring-app/src/main/kotlin/io/microconfig/spring/client/SpringApp.kt
@@ -16,8 +16,12 @@ fun main(args: Array<String>) {
 @Configuration
 class Config {
     @Bean
-    fun foo(@Value("\${foo}") foo: String): String {
-        println("Value fetched from remote: $foo")
+    fun foo(
+        @Value("\${foo}") foo: String,
+        @Value("\${remote.prop1}") prop1: String,
+        @Value("\${remote.prop2}") prop2: String
+    ): String {
+        println("Value fetched from remote: $foo, $prop1, $prop2")
         return foo
     }
 }
--- spring-client/src/main/kotlin/io/microconfig/spring/client/MicroconfigPostProcessor.kt
+++ spring-client/src/main/kotlin/io/microconfig/spring/client/MicroconfigPostProcessor.kt
@@ -1,13 +1,8 @@
 package io.microconfig.spring.client
 
-import io.microconfig.server.client.ClientConfig
-import io.microconfig.server.client.MicroconfigClient
-import io.microconfig.server.client.MicroconfigClientImpl
-import io.microconfig.server.client.MicroconfigRequest
 import io.microconfig.server.client.dto.ServiceTemplate
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.env.EnvironmentPostProcessor
-import org.springframework.core.annotation.Order
 import org.springframework.core.env.ConfigurableEnvironment
 import java.io.File
 import java.nio.file.Files.createDirectory
@@ -15,33 +10,19 @@ import java.nio.file.Files.writeString
 import java.nio.file.StandardOpenOption.CREATE
 import java.nio.file.StandardOpenOption.TRUNCATE_EXISTING
 import java.nio.file.StandardOpenOption.WRITE
-import java.time.Duration
 
-@Order
-class MicroconfigPostProcessor : EnvironmentPostProcessor {
+class MicroconfigPostProcessor(private val client: MicroconfigSpringClient) : EnvironmentPostProcessor {
+    constructor() : this(MicroconfigSpringClient())
 
     override fun postProcessEnvironment(environment: ConfigurableEnvironment, application: SpringApplication) {
         val name = environment.getProperty("spring.application.name") ?: return
-        val enabled = environment.getProperty("spring.microconfig.enabled") ?: return
+        val enabled = environment.getProperty("microconfig.enabled") ?: return
         if (enabled != "true") return
 
-        val config = client(environment).configMaps(request(name, environment)).first()
+        val config = client.fetchConfig(name, environment)
         val configDir = File("config")
         saveTemplates(configDir, config.templates)
         environment.propertySources.addLast(MicroconfigPropertySource(config.content))
-    }
-
-    private fun request(name: String, environment: ConfigurableEnvironment): MicroconfigRequest {
-        val env = environment.getProperty("spring.microconfig.env") ?: "default"
-        println("Microconfig enabled for $name in $env environment")
-        val ref = environment.getProperty("spring.microconfig.ref")
-        return MicroconfigRequest(name, env, "app", ref)
-    }
-
-    private fun client(environment: ConfigurableEnvironment): MicroconfigClient {
-        val server = environment.getProperty("spring.microconfig.server") ?: "http://localhost:8080"
-        val timeout = Duration.ofSeconds(1)
-        return MicroconfigClientImpl(ClientConfig(server, timeout))
     }
 
     private fun saveTemplates(configDir: File, templates: List<ServiceTemplate>) {
