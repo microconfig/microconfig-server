252d521f1460dfdc68067e13721af440e3d78a11
Evgenii Sladkovskii <kapodes@gmail.com>
2021-03-01 20:38:15 +0100

    moving spring client to separate repo

--- spring-client/src/main/kotlin/io/microconfig/spring/sample/MicroconfigPropertySource.kt
+++
@@ -1,5 +0,0 @@
-package io.microconfig.spring.sample
-
-import org.springframework.core.env.MapPropertySource
-
-class MicroconfigPropertySource(config: Map<String, Any>) : MapPropertySource("microconfig", config)
