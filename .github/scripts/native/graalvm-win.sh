f1c0ce41ad4f9cf009ed245b72e78d64bd97aedf
Evgenii Sladkovskii <kapodes@gmail.com>
2021-03-06 17:39:52 +0100

    graalvm 21 in github actions

--- .github/scripts/native/graalvm-win.sh
+++ .github/scripts/native/graalvm-win.sh
@@ -1,7 +1,9 @@
-curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.2.0/graalvm-ce-java11-windows-amd64-20.2.0.zip -o graalvm.zip
+VERSION=21.0.0
+
+curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-$VERSION/graalvm-ce-java11-windows-amd64-$VERSION.zip -o graalvm.zip
 unzip -q graalvm.zip -d graalvm
 rm graalvm.zip
-mv graalvm/graalvm-ce-java11-20.2.0/* graalvm/
-rm -rf graalvm/graalvm-ce-java11-20.2.0
+mv graalvm/graalvm-ce-java11-*/* graalvm/
+rm -rf graalvm/graalvm-ce-java11-*
 graalvm/bin/gu.cmd install -n native-image
 graalvm/bin/native-image.cmd --version
