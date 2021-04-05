914bcfe09be15056aa6686f761bfb4c48bc11ddb
Evgenii Sladkovskii <kapodes@gmail.com>
2021-04-05 16:22:35 +0200

    maven publishing setup

--- server-common/src/test/kotlin/io/microconfig/server/common/SugarKtTest.kt
+++ server-common/src/test/kotlin/io/microconfig/server/common/SugarKtTest.kt
@@ -2,14 +2,12 @@ package io.microconfig.server.common
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import java.util.*
+import java.util.UUID
 
 internal class SugarKtTest {
     @Test
     fun should_deser_uuid() {
-        val json = """{
-                        "id": "3d470e1c-28d6-421e-85f7-5c5220360fc2"
-                      }""".json()
+        val json = """{"id": "3d470e1c-28d6-421e-85f7-5c5220360fc2"}""".json()
         val uuid = json.uuid("id")
 
         assertEquals(UUID.fromString("3d470e1c-28d6-421e-85f7-5c5220360fc2"), uuid)
